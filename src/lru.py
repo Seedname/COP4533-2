6b410d7f239d75959a49290faab32ceee42e8787
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2026-03-06 14:14:52 -0500

    fix lru

--- src/lru.py
+++ src/lru.py
@@ -2,15 +2,15 @@ from parse import read_input
 
 
 def lru(k, m, requests) -> int:
-    # use dictionary for lookup and storing usage
+    # use dictionary for lookup and storing last usage
     cache = {}
 
     misses = 0
 
-    for r in requests:
-        # if r is in the cache, increment its usage and continue
+    for i, r in enumerate(requests):
+        # if r is in the cache, update its usage and continue
         if r in cache:
-            cache[r] += 1
+            cache[r] = i
             continue
 
         # else, its a miss
@@ -18,16 +18,16 @@ def lru(k, m, requests) -> int:
 
         # if the cache isnt full, add the item to the cache
         if len(cache) < k:
-            cache[r] = 0
+            cache[r] = i
 
         # if the cache is full, find the item that 
-        # has been accessed the least and remove it from the cache
+        # where its been the longest since use and remove it from the cache
         item, _ = min(list(cache.items()), key=lambda x: x[1])
         
         del cache[item]
 
         # add the new item to the cache
-        cache[r] = 0
+        cache[r] = i
 
     # return the number of misses
     return misses
